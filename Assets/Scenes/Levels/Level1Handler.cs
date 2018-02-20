c3c00a88f951ad3689f3b1b90eae416c872478c7
Fredrik Tillman <fredrik.tillman@gmail.com>
2018-02-20 22:37:24 +0100

    Edited level

--- Assets/Scenes/Levels/Level1Handler.cs
+++ Assets/Scenes/Levels/Level1Handler.cs
@@ -5,8 +5,8 @@ namespace AlienShooter.Levels {
 	public class Level1Handler : LevelHandlerBase {
 
 		new void Start(){
-			enemies[0].fireRate = 30;
-			enemies[0].chanceOfFire = 3;
+			enemies[0].fireRate = 50;
+			enemies[0].chanceOfFire = 2;
 			
 
 			base.Start();
@@ -16,13 +16,7 @@ namespace AlienShooter.Levels {
 			var w = new List<EnemyWave>();
 			w.Add(helper.NineTimesFourSimpleWave());
 			w.Add(helper.NineTimesFourSimpleWave());
-			w.Add(helper.NineBadGuy1InAStraightLine());
-			// w.Add(helper.NineShipsInASlash());
-			// w.Add(helper.NineShipsInABackSlash());
-			// w.Add(helper.SevenShipsInAV());
-			// w.Add(helper.NineShipsInASlash(0, 150));
-			// w.Add(helper.NineShipsInABackSlash());
-			// w.Add(helper.SevenShipsInAV(0, 400));
+			w.Add(helper.NineTimesFourSimpleWave());
 			waves = w.ToArray();
 		}
 	}
